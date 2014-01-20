c3345fec497e8805c397154baa4bc9233381f4ca
Mark Nemec <mrknmc@me.com>
2014-01-20 13:44:20 +0000

    adding hello world project

---
+++ robot/src/main/java/group2/sdp/App.java
@@ -0,0 +1,14 @@
+package group2.sdp;
+import lejos.nxt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
