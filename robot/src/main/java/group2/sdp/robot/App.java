a7ab691eec0c9608d6ccac3faa88e1c31d3b3978
MatthiasGu <lordmotiejus@yahoo.com>
2014-01-22 17:35:15 +0000

    Update App.java

--- robot/src/main/java/group2/sdp/robot/App.java
+++ robot/src/main/java/group2/sdp/robot/App.java
@@ -3,11 +3,31 @@ package group2.sdp.robot;
 import lejos.nxt.Button;
 
 public class App {
-
+	
 	public static void main(String[] args) {
 		Pilot pilot = new Pilot();
-		pilot.moveForward(5);
-		Button.waitForAnyPress();
+		while(true){
+		while(pilot.leftSensor.readValue() > 37 && pilot. rightSensor.readValue() < 37) {
+		pilot.setRotateSpeed(75);
+		pilot.rotateRight();
+		}
+		while(pilot.rightSensor.readValue() > 37 && pilot.leftSensor.readValue() < 37) {
+		pilot.setRotateSpeed(75);
+		pilot.rotateLeft();			
+		}
+		while(pilot.rightSensor.readValue() > 37 && pilot.leftSensor.readValue() > 37) {
+		pilot.arcBackward(45);
+		pilot.setRotateSpeed(75);
+		pilot.rotateRight();
+		}
+		
+		while(pilot.leftSensor.readValue() < 37 && pilot.rightSensor.readValue() < 37) {
+		pilot.forward(); 
+		}
+			
+		}
+			
+		
+		
 	}
-
 }
