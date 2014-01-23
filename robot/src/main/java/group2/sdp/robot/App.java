2904151d536d0e0969af6fb7bc172198593eccb9
Gordon <Gordon@dunscore.inf.ed.ac.uk>
2014-01-23 17:45:20 +0000

    Added a kick class

--- robot/src/main/java/group2/sdp/robot/App.java
+++ robot/src/main/java/group2/sdp/robot/App.java
@@ -1,31 +1,36 @@
 package group2.sdp.robot;
 
+import lejos.nxt.Button;
+
 
 public class App {
 	
 	public static void main(String[] args) {
-		Pilot pilot = new Pilot();
-		while(true){
-		while(pilot.leftSensor.readValue() > 37 && pilot. rightSensor.readValue() < 37) {
-			pilot.setRotateSpeed(75);
-			pilot.rotateRight();
-		}
-		while(pilot.rightSensor.readValue() > 37 && pilot.leftSensor.readValue() < 37) {
-			pilot.setRotateSpeed(75);
-			pilot.rotateLeft();			
-		}
-		while(pilot.rightSensor.readValue() > 37 && pilot.leftSensor.readValue() > 37) {
-			pilot.arcBackward(45);
-			pilot.setRotateSpeed(75);
-			pilot.rotateRight();
-		}
-		
-		while(pilot.leftSensor.readValue() < 37 && pilot.rightSensor.readValue() < 37) {
-			pilot.forward(); 
-		}
+//		Pilot pilot = new Pilot();
+		System.out.print("done");
+		new Kicker();
+//		while(true){
+//		while(pilot.leftSensor.readValue() > 37 && pilot. rightSensor.readValue() < 37) {
+//			pilot.setRotateSpeed(75);
+//			pilot.rotateRight();
+//		}
+//		while(pilot.rightSensor.readValue() > 37 && pilot.leftSensor.readValue() < 37) {
+//			pilot.setRotateSpeed(75);
+//			pilot.rotateLeft();			
+//		}
+//		while(pilot.rightSensor.readValue() > 37 && pilot.leftSensor.readValue() > 37) {
+//			pilot.arcBackward(45);
+//			pilot.setRotateSpeed(75);
+//			pilot.rotateRight();
+//		}
+//		
+//		while(pilot.leftSensor.readValue() < 37 && pilot.rightSensor.readValue() < 37) {
+//			pilot.forward(); 
+//		}
+//			
+//		}
 			
-		}
-			
+		Button.waitForAnyPress();
 		
 	}
 }
--- robot/src/main/java/group2/sdp/robot/Kicker.java
+++ robot/src/main/java/group2/sdp/robot/Kicker.java
@@ -1,11 +1,11 @@
 package group2.sdp.robot;
 
 import lejos.nxt.Motor;
-import lejos.robotics.navigation.DifferentialPilot;
 
-public class Kicker extends DifferentialPilot{
-	public Kicker() {
-		super(Pilot.WHEEL_SIZE_NXT2, 4.4f, Motor.C, Motor.A, false);
-		
+public class Kicker {
+	public static void kick( int angle, int speed) {
+		Motor.B.setSpeed(speed);
+		Motor.B.rotate(angle);
+		Motor.B.rotate(-angle);
 	}
 }
