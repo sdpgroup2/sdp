d3187c941935a9defb1e8db76acdda9be060a666
Gordon <Gordon@dunscore.inf.ed.ac.uk>
2014-01-23 18:15:42 +0000

    added kicker class

--- robot/src/main/java/group2/sdp/robot/Kicker.java
+++ robot/src/main/java/group2/sdp/robot/Kicker.java
@@ -3,7 +3,7 @@ package group2.sdp.robot;
 import lejos.nxt.Motor;
 
 public class Kicker {
-	public static void kick( int angle, int speed) {
+	public static void kick(int angle, int speed) {
 		Motor.B.setSpeed(speed);
 		Motor.B.rotate(angle);
 		Motor.B.rotate(-angle);
