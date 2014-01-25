c0713e679514bbea59f6b95626881c0a4613e08f
Gordon <Gordon@dunscore.inf.ed.ac.uk>
2014-01-25 17:48:46 +0000

    local

--- robot/src/main/java/group2/sdp/robot/App.java
+++ robot/src/main/java/group2/sdp/robot/App.java
@@ -1,36 +1,76 @@
 package group2.sdp.robot;
 
 import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
 
 
 public class App {
 	
+	private static boolean rotateRight;
+	private static boolean rotateLeft;
+	private static boolean exit;
+	private static double priorDistance;
+
 	public static void main(String[] args) {
 		Pilot pilot = new Pilot();
 		System.out.print("done");
-
-		while(true){
-		while(pilot.leftSensor.readValue() > 37 && pilot. rightSensor.readValue() < 37) {
-			pilot.setRotateSpeed(75);
-			pilot.rotateRight();
-		}
-		while(pilot.rightSensor.readValue() > 37 && pilot.leftSensor.readValue() < 37) {
-			pilot.setRotateSpeed(75);
-			pilot.rotateLeft();			
-		}
-		while(pilot.rightSensor.readValue() > 37 && pilot.leftSensor.readValue() > 37) {
-			pilot.arcBackward(45);
-			pilot.setRotateSpeed(75);
-			pilot.rotateRight();
-		}
+		rotateRight = false;
+		rotateLeft = false;
+		exit = false;
 		
-		while(pilot.leftSensor.readValue() < 37 && pilot.rightSensor.readValue() < 37) {
-			pilot.forward(); 
-		}
+		while(!exit){
+			if((pilot.leftSensor.readValue() > 37 && pilot.rightSensor.readValue() < 37)
+					) {
+//				pilot.setRotateSpeed(40);
+//				pilot.rotateRight();
+				rotateRight = true;
+				priorDistance = (Motor.A.getTachoCount() + Motor.C.getTachoCount())/2;
+				exit = true;
+			}
+			if(pilot.rightSensor.readValue() > 37 && pilot.leftSensor.readValue() < 37) {
+//				pilot.setRotateSpeed(40);
+//				pilot.rotateLeft();		Value() > 37 || pilot.leftSensor.readValue() > 37)) {	
+				rotateLeft = true;
+				priorDistance = (Motor.A.getTachoCount() + Motor.C.getTachoCount())/2;
+				exit = true;
+//				System.out.println("left");
+			}
+//			if(pilot.rightSensor.readValue() > 37 && pilot.leftSensor.readValue() > 37) {
+////				pilot.arcBackward(45);
+////				pilot.setRotateSpeed(55);
+////				pilot.rotateRight();
+//				rotateRight = true;
+//				exit = true;
+//			}
+			
+			if(pilot.leftSensor.readValue() < 37 && pilot.rightSensor.readValue() < 37) {
+				pilot.setTravelSpeed(50);
+				pilot.forward(); 
+			}
 			
 		}
+		boolean stop = false;
+		while (exit && !stop) {
+			if(rotateRight && (pilot.rightSensor.readValue() > 37 || pilot.leftSensor.readValue() > 37)) {
+				// pilot.arcBackward(30);
+				pilot.setRotateSpeed(60);
+				pilot.rotateRight();
+			}
+			if(rotateLeft && (pilot.rightSensor.readValue() > 37 || pilot.leftSensor.readValue() > 37)) {
+				// pilot.arcBackward(-30);
+				pilot.setRotateSpeed(60);
+				pilot.rotateLeft();			
+			}
 			
-		
-		
+			if(pilot.leftSensor.readValue() < 37 && pilot.rightSensor.readValue() < 37) {
+				pilot.setTravelSpeed(100);
+				pilot.forward(); 
+			}
+			if (((Motor.A.getTachoCount() + Motor.C.getTachoCount())/2)-priorDistance >4900) {
+				pilot.stop();
+			}
+//			LCD.drawInt((int) Math.round((((Motor.A.getTachoCount() + Motor.C.getTachoCount())/2)-priorDistance)),0,0);
+		}	
 	}
 }
--- robot/src/main/java/group2/sdp/robot/Pilot.java
+++ robot/src/main/java/group2/sdp/robot/Pilot.java
@@ -22,19 +22,13 @@ public class Pilot extends DifferentialPilot implements LightListener {
 	}
 
 	public Pilot() {
-		super(Pilot.WHEEL_SIZE_NXT2, 4.4f, Motor.C, Motor.A, false);
+		super(56, 98, Motor.C, Motor.A, false);
 		this.leftSensor = new MyLightSensor(SensorPort.S1, 100);
-		this.leftSensor.addListener(this);
 		this.rightSensor = new MyLightSensor(SensorPort.S2, 100);
-		this.rightSensor.addListener(this);
 	}
 
 	public void lightMeasured(int lightVal, MyLightSensor sensor) {
 		System.out.println(lightVal);
-		if (lightVal > WHITE_THRESHOLD) {
-			System.out.println(lightVal);
-			this.stop();
-		}
 	}
 
 }
