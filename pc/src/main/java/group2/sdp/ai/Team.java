e04f9d3c42565912adcc7c8b43609584fcbc197c
draco <j@hirniak.info>
2014-02-07 12:47:35 +0000

    Added team class. [NXTGeneral]

---
+++ pc/src/main/java/group2/sdp/ai/Team.java
@@ -0,0 +1,15 @@
+package group2.sdp.ai;
+
+/** Contains representation of a state from a team point of view.
+ *  Hence, the same for ally and enemy. */
+public class Team
+{
+    private boolean ally;
+    // add planes for defence, attack enhanced by robot information
+    
+    public Team(boolean ally)
+    { this.ally = ally; }
+    
+    public boolean isAlly()
+    { return ally; }
+}
