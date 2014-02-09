ec152699ebdeafe03ea6f66fa3b2c85ac9ea54b9
Mark Nemec <mrknmc@me.com>
2014-02-09 20:56:57 +0000

    updating DotCluster color thresholds

--- pc/src/main/java/group2/sdp/pc/vision/clusters/DotCluster.java
+++ pc/src/main/java/group2/sdp/pc/vision/clusters/DotCluster.java
@@ -10,12 +10,13 @@ import java.util.List;
 public class DotCluster extends HSBCluster {
 
 	public DotCluster(String name) {
-		super(name, new HSBColor(60, 20, 20), new HSBColor(120, 38, 38), Color.magenta);
+		super(name, new HSBColor(38, 15, 15), new HSBColor(130, 33, 33), Color.ORANGE);
+//		super(name, new HSBColor(112, 20, 22), new HSBColor(15, 33, 33), Color.ORANGE);
 	}
 
 	@Override
 	public List<Rect> getImportantRects() {
-		return getRects(2, 20, 2, 20, 0.5f, 1.1f);
+		return getRects(2, 10, 2, 10, 0.5f, Float.MAX_VALUE);
 	}
-
+	
 }
