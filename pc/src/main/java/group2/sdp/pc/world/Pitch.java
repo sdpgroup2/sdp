d45a8eb6dd11a12da65fabf98588ddc3f58267e3
Mark Nemec <mrknmc@me.com>
2014-02-10 19:24:45 +0000

    added getOurDefender, getTheirAttacker etc.

--- pc/src/main/java/group2/sdp/pc/world/Pitch.java
+++ pc/src/main/java/group2/sdp/pc/world/Pitch.java
@@ -11,13 +11,8 @@ import java.util.List;
 
 public class Pitch {
 
-	public Rect getPitchRect() {
-		return pitchRect;
-	}
-
-	public void setPitchRect(Rect pitchRect) {
-		this.pitchRect = pitchRect;
-	}
+	public static TeamColor ourTeam;
+	public static PitchType pitchPlayed;
 
 	private PitchLinesCluster lines;
 	private PitchSectionCluster sections;
@@ -28,10 +23,15 @@ public class Pitch {
 	private Rect leftDefenseZone;
 	private Rect rightDefenseZone;
 
-	private Robot leftDefender;
-	private Robot leftAttacker;
-	private Robot rightDefender;
-	private Robot rightAttacker;
+	// private Robot leftDefender;
+	// private Robot leftAttacker;
+	// private Robot rightDefender;
+	// private Robot rightAttacker;
+
+	private Robot yellowDefender;
+	private Robot yellowAttacker;
+	private Robot blueDefender;
+	private Robot blueAttacker;
 
 	private Ball ball;
 
@@ -63,43 +63,70 @@ public class Pitch {
 	 * @param blueRobots - List of rectangles of blue robots.
 	 * @param yellowRobots - List of rectangles of yellow robots.
 	 */
+	// public void addRobots(List<Robot> blueRobots, List<Robot> yellowRobots, TeamColor ourTeam) {
+	// 	Robot firstBlueRobot = blueRobots.get(0);
+	// 	Robot secondBlueRobot = blueRobots.get(1);
+	// 	Robot firstYellowRobot = yellowRobots.get(0);
+	// 	Robot secondYellowRobot = yellowRobots.get(1);
+
+	// 	if (this.leftDefenseZone.contains(firstBlueRobot.getPosition())) {
+	// 		this.leftDefender = firstBlueRobot;
+	// 		this.leftAttacker = secondBlueRobot;
+	// 		if (this.rightDefenseZone.contains(firstYellowRobot.getPosition())) {
+	// 			this.rightDefender = firstYellowRobot;
+	// 			this.rightAttacker = secondYellowRobot;
+	// 		}
+	// 	} else if (this.leftAttackZone.contains(firstBlueRobot.getPosition())) {
+	// 		this.leftAttacker = firstBlueRobot;
+	// 		this.leftDefender = secondBlueRobot;
+	// 		if (this.rightDefenseZone.contains(firstYellowRobot.getPosition())) {
+	// 			this.rightDefender = firstYellowRobot;
+	// 			this.rightAttacker = secondYellowRobot;
+	// 		}
+	// 	} else if (this.rightDefenseZone.contains(firstBlueRobot.getPosition())) {
+	// 		this.rightDefender = firstBlueRobot;
+	// 		this.rightAttacker = secondBlueRobot;
+	// 		if (this.leftDefenseZone.contains(firstYellowRobot.getPosition())) {
+	// 			this.leftDefender = firstYellowRobot;
+	// 			this.leftAttacker = secondYellowRobot;
+	// 		}
+	// 	} else {
+	// 		this.rightAttacker = firstBlueRobot;
+	// 		this.rightDefender = secondBlueRobot;
+	// 		if (this.leftDefenseZone.contains(firstYellowRobot.getPosition())) {
+	// 			this.leftDefender = firstYellowRobot;
+	// 			this.leftAttacker = secondYellowRobot;
+	// 		}
+	// 	}
+	// }
+
 	public void addRobots(List<Robot> blueRobots, List<Robot> yellowRobots, TeamColor ourTeam) {
 		Robot firstBlueRobot = blueRobots.get(0);
 		Robot secondBlueRobot = blueRobots.get(1);
 		Robot firstYellowRobot = yellowRobots.get(0);
 		Robot secondYellowRobot = yellowRobots.get(1);
 
-		if (this.leftDefenseZone.contains(firstBlueRobot.getPosition())) {
-			this.leftDefender = firstBlueRobot;
-			this.leftAttacker = secondBlueRobot;
-			if (this.rightDefenseZone.contains(firstYellowRobot.getPosition())) {
-				this.rightDefender = firstYellowRobot;
-				this.rightAttacker = secondYellowRobot;
-			}
-		} else if (this.leftAttackZone.contains(firstBlueRobot.getPosition())) {
-			this.leftAttacker = firstBlueRobot;
-			this.leftDefender = secondBlueRobot;
-			if (this.rightDefenseZone.contains(firstYellowRobot.getPosition())) {
-				this.rightDefender = firstYellowRobot;
-				this.rightAttacker = secondYellowRobot;
-			}
-		} else if (this.rightDefenseZone.contains(firstBlueRobot.getPosition())) {
-			this.rightDefender = firstBlueRobot;
-			this.rightAttacker = secondBlueRobot;
-			if (this.leftDefenseZone.contains(firstYellowRobot.getPosition())) {
-				this.leftDefender = firstYellowRobot;
-				this.leftAttacker = secondYellowRobot;
-			}
+		if (this.leftDefenseZone.contains(firstBlueRobot.getPosition()) ||
+			this.rightDefenseZone.contains(firstBlueRobot.getPosition)) {
+			this.blueDefender = firstBlueRobot;
+			this.blueAttacker = secondBlueRobot;
 		} else {
-			this.rightAttacker = firstBlueRobot;
-			this.rightDefender = secondBlueRobot;
-			if (this.leftDefenseZone.contains(firstYellowRobot.getPosition())) {
-				this.leftDefender = firstYellowRobot;
-				this.leftAttacker = secondYellowRobot;
-			}
+			this.blueAttacker = firstBlueRobot;
+			this.blueDefender = secondBlueRobot;
+		}
+		if (this.leftAttackZone.contains(firstYellowRobot.getPosition()) ||
+			this.rightAttackZone.contains(firstYellowRobot.getPosition())) {
+			this.yellowAttacker = firstYellowRobot;
+			this.yellowDefender = secondYellowRobot;
+		} else {
+			this.yellowDefender = firstYellowRobot;
+			this.yellowAttacker = secondYellowRobot;
 		}
 	}
 
+	/**
+	 * Translate center point from the Vision coordinate system to a coordinate system of this pitch.
+	 */
 	public void translate(StaticObject object) {
 		Point translatedPt = CoordinateTranslator.fromVisionToPitch(object.getPosition(), this);
 		object.setPosition(translatedPt);
@@ -115,5 +142,42 @@ public class Pitch {
         }
        return result;
 	}
+
+	/**
+	 * Returns the defender of our team
+	 */
+	public Robot getOurDefender() {
+		return ourTeam == TeamColor.YELLOW ? this.yellowDefender : this.yellowAttacker;
+	}
+
+	/**
+	 * Returns the attacker of our team
+	 */
+	public Robot getOurAttacker() {
+		return ourTeam == TeamColor.YELLOW ? this.yellowAttacker : this.yellowDefender;
+	}
+
+	/**
+	 * Returns the defender of the other team
+	 */
+	public Robot getTheirDefender() {
+		return ourTeam != TeamColor.YELLOW ? this.yellowDefender : this.yellowAttacker;
+	}
+
+	/**
+	 * Returns the attacker of the other team
+	 */
+	public Robot getTheirAttacker() {
+		return ourTeam != TeamColor.YELLOW ? this.yellowAttacker : this.yellowDefender;
+	}
+
+	public Rect getPitchRect() {
+		return pitchRect;
+	}
+
+	public void setPitchRect(Rect pitchRect) {
+		this.pitchRect = pitchRect;
+	}
+
 
 }
