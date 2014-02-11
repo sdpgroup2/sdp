53df8873ee9e192896bc14fe6b713a492ca1a84f
Gordon <Gordon@dunscore.inf.ed.ac.uk>
2014-02-11 19:38:01 +0000

    adding command queue working with one robot, not tested yet with 2

--- pc/src/main/java/group2/sdp/pc/world/Constants.java
+++ pc/src/main/java/group2/sdp/pc/world/Constants.java
@@ -6,7 +6,12 @@ public class Constants {
 	public static final int SIDE_PITCH = -1;
 	public static final int YELLOW_TEAM = 1;
 	public static final int BLUE_TEAM = -1;
-
+	
+	public static final String ROBOT_2A_NAME = "SDP2A";
+	public static final String ROBOT_2A_MAC = "00165307D55F";
+	public static final String ROBOT_2D_NAME = "SDP 2D";
+	public static final String ROBOT_2D_MAC = "0016530BBBEA";
+	
 	public enum TeamColor {
 		YELLOW,
 		BLUE
