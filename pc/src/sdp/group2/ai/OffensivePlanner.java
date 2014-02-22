c40e933184001652497fcc7cfb1d1365674fe915
mikeyjrowley <Michael@Rowley-cs.co.uk>
2014-02-22 21:49:52 +0000

    Finished More Methods

--- pc/src/sdp/group2/ai/OffensivePlanner.java
+++ pc/src/sdp/group2/ai/OffensivePlanner.java
@@ -11,55 +11,51 @@ import sdp.group2.world.Zone;
 import sdp.group2.world.Ball;
 import sdp.group2.world.IPitch;
 
-
-
 public class OffensivePlanner extends Planner {
 
 	private static final Point GOAL = new Point(0, Plane.pix2mm(70));
 	private static final int SPEED = 100;
-	
+
 	private Zone offensiveZone;
 	private Robot attackerRobot;
 	private boolean isRobotAligned = false;
 	String robotName = "SDP 2A";
 	String robotMacAddress = "CH:UJ";
 	private Sender sender;
-	
-	public OffensivePlanner(IPitch pitch, byte zoneId)
-	{ 
+
+	public OffensivePlanner(IPitch pitch, byte zoneId) {
 		super(pitch);
 		this.offensiveZone = getPitch().getZone(zoneId);
 		this.attackerRobot = offensiveZone.getRobot();
-		
-		try { sender = new Sender(robotName, robotMacAddress); }
-		catch (IOException e) { e.printStackTrace(); }
+
+		try {
+			sender = new Sender(robotName, robotMacAddress);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public boolean isAbleToScore()
-	{ 
+	public boolean isAbleToScore() {
 		Point ballPosition = pitch.getBall().getPosition();
 		Line arrow = offensiveZone.expand(ballPosition, 0.0);
 		Point endpoint = offensiveZone.getIntersection(arrow);
 		double space = Math.abs(endpoint.distance(endpoint));
-		
-		return space >= attackerRobot.getRadius() * 2; 
+
+		return space >= attackerRobot.getRadius() * 2;
 	}
-	 
-	public void score()
-	{
+
+	public void score() {
 		Point ballPosition = pitch.getBall().getPosition();
 		Line line1 = getTargetPath();
-		// project 
+		// project
 		Line line0 = line1.getPerpendicular();
 	}
-	
-	public void passBack()
-	{
-		
+
+	public void passBack() {
+
 	}
-	
-	public Line getTargetPath()
-	{
+
+	public Line getTargetPath() {
 		Ball ball = pitch.getBall();
 		Point ballPosition = ball.getPosition();
 		Line arrow = offensiveZone.expand(GOAL, ballPosition);
@@ -67,25 +63,31 @@ public class OffensivePlanner extends Planner {
 		Line target = arrow.getPerpendicular();
 		return offensiveZone.expand(target);
 	}
-	
-	public void align(double direction)
-	{
+
+	public void align(double direction) {
 		double robotDirection = attackerRobot.getDirection();
-		double theta = Math.abs(direction - Math.abs(robotDirection)); // rotation to align
+		double theta = Math.abs(direction - Math.abs(robotDirection)); // rotation
+																		// to
+																		// align
 		int sign = direction > robotDirection ? 1 : -1;
 		int thetaDeg = sign * Zone.rad2deg(theta);
-		
-		try { sender.rotate(thetaDeg, SPEED); }
-		catch (IOException e) { e.printStackTrace(); }
+
+		try {
+			sender.rotate(thetaDeg, SPEED);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public void act()
-	{
-		if (attackerRobot.isMoving()) { return; }
-		if (isAbleToScore())
-		{ score(); }
-		else
-		{ passBack(); }
+
+	public void act() {
+		if (attackerRobot.isMoving()) {
+			return;
+		}
+		if (isAbleToScore()) {
+			score();
+		} else {
+			passBack();
+		}
 	}
-	
+
 }
